2e877656d074f31aa81409c9445d36f8bf093a9d
Ravi <ravi.yaganti@valuelabs.com>
2017-08-11 16:42:27 +0530

    > Added Alamofire library

--- RKCustomCamera/CustomCamera/ViewController.swift
+++ RKCustomCamera/CustomCamera/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import AVFoundation
 import Foundation
 import CoreImage
+import Alamofire
 
 class ViewController: UIViewController {
     
@@ -33,35 +34,66 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.perform(#selector(self.capturDevice), with: nil, afterDelay: 1.0)
-//
-//        self.capturDevice()
     }
     
     
     override func viewWillAppear(_ animated: Bool) {
+        
     }
     
     //MARK: -- Custom Methods
     func capturDevice(){
-     captureDevice = AVCaptureDevice.defaultDevice(withDeviceType: .builtInWideAngleCamera, mediaType: AVMediaTypeVideo, position: .front)
-        if captureDevice != nil {
-            print("Capture device found")
-            beginSession()
+        self.checkCameraAuthorization { authorized in
+            if authorized {
+                // Proceed to set up and use the camera.
+                self.captureDevice = AVCaptureDevice.defaultDevice(withDeviceType: .builtInWideAngleCamera, mediaType: AVMediaTypeVideo, position: .front)
+                if self.captureDevice != nil {
+                    print("Capture device found")
+                    self.beginSession()
+                }
+            } else {
+                print("Permission to use camera denied.")
+            }
         }
-        
-        
+     
+    }
+    
+    func checkCameraAuthorization(_ completionHandler: @escaping ((_ authorized: Bool) -> Void)) {
+        switch AVCaptureDevice.authorizationStatus(forMediaType: AVMediaTypeVideo) {
+        case .authorized:
+            //The user has previously granted access to the camera.
+            completionHandler(true)
+            
+        case .notDetermined:
+            // The user has not yet been presented with the option to grant video access so request access.
+            AVCaptureDevice.requestAccess(forMediaType: AVMediaTypeVideo, completionHandler: { success in
+                completionHandler(success)
+            })
+            
+        case .denied:
+            // The user has previously denied access.
+            completionHandler(false)
+            
+        case .restricted:
+            // The user doesn't have the authority to request access e.g. parental restriction.
+            completionHandler(false)
+        }
     }
     
     func takePhoto(){
         // Make sure capturePhotoOutput is valid
         guard let capturePhotoOutput = self.capturePhotoOutput else { return }
-        
         // Get an instance of AVCapturePhotoSettings class
         let photoSettings = AVCapturePhotoSettings()
-        
+        let previewPixelType = photoSettings.availablePreviewPhotoPixelFormatTypes.first!
+        let previewFormat = [kCVPixelBufferPixelFormatTypeKey as String: previewPixelType,
+                             kCVPixelBufferWidthKey as String: 160,
+                             kCVPixelBufferHeightKey as String: 160,
+                             ]
+        photoSettings.previewPhotoFormat = previewFormat
         // Set photo settings for our need
-        photoSettings.isAutoStillImageStabilizationEnabled = true
-        photoSettings.isHighResolutionPhotoEnabled = true
+//        photoSettings.isAutoStillImageStabilizationEnabled = true
+//        photoSettings.isHighResolutionPhotoEnabled = true
         // Call capturePhoto method by passing our photo settings and a delegate implementing AVCapturePhotoCaptureDelegate
         capturePhotoOutput.capturePhoto(with: photoSettings, delegate: self)
     }
@@ -124,10 +156,13 @@ class ViewController: UIViewController {
                 self.startTimer()
             }
         }
-     
-
     }
+    
+
     func endSession() {
+        if captureSession.isRunning {
+            captureSession.stopRunning()
+        }
         self.cameraView.removeFromSuperview()
     }
  
@@ -149,18 +184,154 @@ class ViewController: UIViewController {
     deinit {
         self.stopTimer()
     }
+    
+    @IBAction func uploadImage(_ sender: Any) {
+        self.uploadWithAlamofire()
+    }
+    
+    
+    //Not used this Method Added just for reference
+    func configureCaptureSession(_ completionHandler: ((_ success: Bool) -> Void)) {
+        var success = false
+        defer { completionHandler(success) } // Ensure all exit paths call completion handler.
+        
+        // Get video input for the default camera.
+//        let videoCaptureDevice = defaultDevice()
+        guard let videoInput = try? AVCaptureDeviceInput(device: self.captureDevice) else {
+            print("Unable to obtain video input for default camera.")
+            return
+        }
+        
+        // Create and configure the photo output.
+        let capturePhotoOutput = AVCapturePhotoOutput()
+        capturePhotoOutput.isHighResolutionCaptureEnabled = true
+        capturePhotoOutput.isLivePhotoCaptureEnabled = capturePhotoOutput.isLivePhotoCaptureSupported
+        
+        // Make sure inputs and output can be added to session.
+        guard self.captureSession.canAddInput(videoInput) else { return }
+        guard self.captureSession.canAddOutput(capturePhotoOutput) else { return }
+        
+        // Configure the session.
+        self.captureSession.beginConfiguration()
+        self.captureSession.sessionPreset = AVCaptureSessionPresetPhoto
+        self.captureSession.addInput(videoInput)
+        self.captureSession.addOutput(capturePhotoOutput)
+        self.captureSession.commitConfiguration()
+        
+        self.capturePhotoOutput = capturePhotoOutput
+        
+        success = true
+    }
+    
+    
+    
+    //MARK:// API Call
 
+    // import Alamofire
+    func uploadWithAlamofire() {
+        let image = UIImage(named:"1_jhon")
+       // let imageData = UIImagePNGRepresentation(image!)
+        // User "authentication":
+//        let parameters = ["user":"Sol", "password":"secret1234"]
+        
+        // Image to upload:
+        let imageToUploadURL = Bundle.main.url(forResource: "1_jhon", withExtension: "jpg")
+        
+        // Server address (replace this with the address of your own server):
+        let url = ""
+        // Use Alamofire to upload the image
+        Alamofire.upload(
+            multipartFormData: { multipartFormData in
+                // On the PHP side you can retrive the image using $_FILES["image"]["tmp_name"]
+                multipartFormData.append(imageToUploadURL!, withName: "image")
+//                for (key, val) in parameters {
+//                    multipartFormData.append(val.data(using: String.Encoding.utf8)!, withName: key)
+//                }
+        },
+            to: url,
+            encodingCompletion: { encodingResult in
+                switch encodingResult {
+                case .success(let upload, _, _):
+                    upload.responseJSON { response in
+                        if let jsonResponse = response.result.value as? [String: Any] {
+                            print(jsonResponse)
+                        }
+                    }
+                case .failure(let encodingError):
+                    print(encodingError)
+                }
+        }
+        )
+        
+        // Reference
+        //        let image = UIImage(named:"1_jhon")
+        //
+        //        // define parameters
+        //        let parameters = [
+        //            "hometown": "yalikavak",
+        //            //            "living": "istanbul"
+        //        ]
+        //
+        //        Alamofire.upload(multipartFormData: { multipartFormData in
+        //            if let imageData = UIImageJPEGRepresentation(image!, 1) {
+        //                multipartFormData.append(imageData, withName: "file", fileName: "file.png", mimeType: "image/png")
+        //            }
+        //
+        //            for (key, value) in parameters {
+        //                multipartFormData.append((value.data(using: .utf8))!, withName: key)
+        //            }}, to: "upload_url", method: .post, headers: ["Authorization": "auth_token"],
+        //                encodingCompletion: { encodingResult in
+        //                    switch encodingResult {
+        //                    case .success(let upload, _, _):
+        //                        upload.response { [weak self] response in
+        //                            guard self != nil else {
+        //                                return
+        //                            }
+        //                            debugPrint(response)
+        //                        }
+        //                    case .failure(let encodingError):
+        //                        print("error:\(encodingError)")
+        //                    }
+        //        })
+        
+        
+    }
+    
+    
+    
+    func uploadImage() {
+        let image = UIImage(named:"1_jhon")
+        let imageData = UIImagePNGRepresentation(image!)
+        if imageData != nil {
+            Alamofire.upload(imageData!, to:"yoururl").responseJSON { response in
+                debugPrint(response)
+            }
+        }
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     override func viewWillDisappear(_ animated: Bool) {
-//        self.captureSession.stopRunning()
+        super.viewWillDisappear(animated)
+        
+        if captureSession.isRunning {
+            captureSession.stopRunning()
+        }
         
     }
 }
-
+extension NSMutableData {
+    
+    func appendString(string: String) {
+        let data = string.data(using: String.Encoding.utf8, allowLossyConversion: true)
+        append(data!)
+    }
+}
 extension ViewController : AVCapturePhotoCaptureDelegate {
     func capture(_ captureOutput: AVCapturePhotoOutput,
                  didFinishProcessingPhotoSampleBuffer photoSampleBuffer: CMSampleBuffer?,
